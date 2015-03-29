fc7b987fb6eb6a8a17df803dcd2db03e6e345b38
Mohammad Rahhal <m.r992@hotmail.com>
2015-03-29 12:09:29 +0300

    + nuspec

--- Konsola/Properties/AssemblyInfo.cs
+++ Konsola/Properties/AssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Konsola")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Contains services for command line tools.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MRH")]
 [assembly: AssemblyProduct("Konsola")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
