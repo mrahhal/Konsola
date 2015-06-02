75b1522dfc07e34a97e3a04a354245d1ae729ce7
Mohammad Rahhal <m.r992@hotmail.com>
2015-06-02 12:15:31 +0300

    + SilentConsole

--- Konsola/Console.cs
+++ Konsola/Console.cs
@@ -8,6 +8,9 @@ using SConsole = System.Console;
 
 namespace Konsola
 {
+	/// <summary>
+	/// An IConsole implementation that writes to system console.
+	/// </summary>
 	public class Console : IConsole
 	{
 		private ConsoleColor _color;
@@ -52,6 +55,17 @@ namespace Konsola
 				{
 					return 0;
 				}
+			}
+		}
+
+		/// <summary>
+		/// Gets a console that silently consumes messages.
+		/// </summary>
+		public static IConsole Silent
+		{
+			get
+			{
+				return SilentConsole.Singleton;
 			}
 		}
 
@@ -220,6 +234,41 @@ namespace Konsola
 			{
 				_color = prevColor;
 			}
+		}
+
+		private class SilentConsole : IConsole
+		{
+			public static readonly SilentConsole Singleton;
+
+			static SilentConsole()
+			{
+				Singleton = new SilentConsole();
+			}
+
+			private SilentConsole()
+			{
+			}
+
+			public void Write(string value) { }
+			public void Write(string format, params object[] args) { }
+			public void WriteLine() { }
+			public void WriteLine(string value) { }
+			public void WriteLine(string format, params object[] args) { }
+			public void WriteWarning(string value) { }
+			public void WriteWarning(string format, params object[] args) { }
+			public void WriteWarningLine(string value) { }
+			public void WriteWarningLine(string format, params object[] args) { }
+			public void WriteError(string value) { }
+			public void WriteError(string format, params object[] args) { }
+			public void WriteErrorLine(string value) { }
+			public void WriteErrorLine(string format, params object[] args) { }
+			public void WriteColor(ConsoleColor color, string value) { }
+			public void WriteColor(ConsoleColor color, string format, params object[] args) { }
+			public void WriteColorLine(ConsoleColor color, string value) { }
+			public void WriteColorLine(ConsoleColor color, string format, params object[] args) { }
+			public void WriteJustified(int startIndex, string value) { }
+			public void WriteJustified(int startIndex, string value, int maxWidth) { }
+			public void WriteJustified(ConsoleColor color, int startIndex, string value, int maxWidth) { }
 		}
 	}
 }
