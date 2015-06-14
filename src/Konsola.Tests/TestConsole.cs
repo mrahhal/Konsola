ffcfa83d988ced715b86f32665c784edd27554c4
Mohammad Rahhal <m.r992@hotmail.com>
2015-06-14 17:21:34 +0300

    + TestConsole

---
+++ src/Konsola.Tests/TestConsole.cs
@@ -0,0 +1,36 @@
+﻿//------------------------------------------------------------------------------
+// Copyright (c) 2015, Mohammad Rahhal @mrahhal
+//------------------------------------------------------------------------------
+
+using System;
+using System.Text;
+
+namespace Konsola.Tests
+{
+	public class TestConsole : IConsole
+	{
+		private StringBuilder _sb = new StringBuilder();
+
+		public string Text { get { return _sb.ToString(); } }
+
+		public int WindowWidth
+		{
+			get { return -1; }
+		}
+
+		public int CursorLeft
+		{
+			get { return -1; }
+		}
+
+		public void Write(WriteKind kind, string value)
+		{
+			_sb.Append(value);
+		}
+
+		public void WriteLine(WriteKind kind, string value)
+		{
+			Write(kind, value + Environment.NewLine);
+		}
+	}
+}
