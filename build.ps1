0ec51fe289003d8644fc1619489d26e6bcd666c3
Mohammad Rahhal <m.r992@hotmail.com>
2015-06-21 18:30:47 +0300

    * build script

--- build.ps1
+++ build.ps1
@@ -1,18 +1,1 @@
-$konsola_nuspec = "nuspecs\Konsola.nuspec"
-$nugetDir = "_nupkg"
-
-Function mkdirb($path)
-{
-    if (!(Test-Path $path))
-    {
-        mkdir $path > $null
-    }
-}
-
-mkdirb $nugetDir
-
-# Build for Release
-msbuild Konsola.sln /nologo /verbosity:minimal /property:Configuration=Release
-
-# Build nuget packages
-nuget pack $konsola_nuspec -Properties Configuration=Release -OutputDirectory $nugetDir
+msbuild build\Build.proj /t:BuildPackages
