811a0d4c4a851f318166ac722ad6f39a308f0b22
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-10 20:10:13 +0300

    + Boxes

---
+++ src/Konsola/Boxes.cs
@@ -0,0 +1,20 @@
+﻿//------------------------------------------------------------------------------
+// Copyright (c) 2015, Mohammad Rahhal @mrahhal
+//------------------------------------------------------------------------------
+
+using System;
+
+namespace Konsola
+{
+	public static class Boxes
+	{
+		static Boxes()
+		{
+			False = (object)false;
+			True = (object)true;
+		}
+
+		public static object False { get; private set; }
+		public static object True { get; private set; }
+	}
+}
