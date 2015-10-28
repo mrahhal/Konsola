d2fb097bb0d6af84fec97a1631e16ef06439925c
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-28 13:09:12 +0200

    + StateMachine

---
+++ src/Konsola/StateMachine.cs
@@ -0,0 +1,126 @@
+using System;
+
+namespace Konsola
+{
+	public class StateMachine<T>
+		where T : class
+	{
+		public StateMachine(T[] data)
+		{
+			if (data == null)
+			{
+				throw new ArgumentNullException("data");
+			}
+
+			Data = data;
+			Position = -1;
+		}
+
+		public T[] Data { get; private set; }
+
+		public int Position { get; set; }
+
+		public bool HasNext
+		{
+			get
+			{
+				return Position + 1 < Data.Length;
+			}
+		}
+
+		public bool HasPrevious
+		{
+			get
+			{
+				return Position - 1 >= 0;
+			}
+		}
+
+		public T Current
+		{
+			get
+			{
+				if (Position >= 0 && Position < Data.Length)
+				{
+					return Data[Position];
+				}
+				return null;
+			}
+		}
+
+		public void Reset()
+		{
+			Position = -1;
+		}
+
+		public T PeekNext()
+		{
+			var next = Position + 1;
+			if (!HasNext)
+			{
+				return null;
+			}
+			return Data[next];
+		}
+
+		public T PeekPrevious()
+		{
+			var previous = Position - 1;
+			if (!HasPrevious)
+			{
+				return null;
+			}
+			return Data[previous];
+		}
+
+		public T Next()
+		{
+			if (!HasNext)
+			{
+				return null;
+			}
+			return Data[++Position];
+		}
+
+		public T Previous()
+		{
+			if (!HasPrevious)
+			{
+				return null;
+			}
+			return Data[--Position];
+		}
+
+		public void VisitAllNext(Action<int, T> visit)
+		{
+			if (visit == null)
+			{
+				throw new ArgumentNullException("visit");
+			}
+			if (!HasNext)
+			{
+				return;
+			}
+			for (int i = Position + 1; i < Data.Length; i++)
+			{
+				visit(i, Data[i]);
+			}
+		}
+
+		public void VisitAllPrevious(Action<int, T> visit)
+		{
+			if (visit == null)
+			{
+				throw new ArgumentNullException("visit");
+			}
+			if (!HasPrevious)
+			{
+				return;
+			}
+			for (int i = Position - 1; i >= 0; i--)
+			{
+				visit(i, Data[i]);
+			}
+		}
+	}
+}
---
+++ test/Konsola.Tests/StateMachineTests.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Xunit;
+
+namespace Konsola
+{
+	public class StateMachineTests : IDisposable
+	{
+		[Fact]
+		public void HandlesNull()
+		{
+			// Arrange + Act + Assert
+			Assert.Throws<ArgumentNullException>(() =>
+				{
+					var sm = new StateMachine<Test>(null);
+				});
+		}
+
+		[Fact]
+		public void HasNext()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+
+			// Act + Assert
+			Assert.True(sm.HasNext);
+		}
+
+		[Fact]
+		public void HasNext_Not()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Position = 1;
+
+			// Act + Assert
+			Assert.False(sm.HasNext);
+		}
+
+		[Fact]
+		public void HasPrevious()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Position = 0;
+
+			// Act + Assert
+			Assert.True(sm.HasNext);
+		}
+
+		[Fact]
+		public void HasPrevious_Not()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+
+			// Act + Assert
+			Assert.False(sm.HasPrevious);
+		}
+
+		[Fact]
+		public void Position()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Next();
+
+			// Act + Assert
+			Assert.Equal(0, sm.Position);
+		}
+
+		[Fact]
+		public void Reset()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Next();
+
+			// Act
+			sm.Reset();
+
+			// Assert
+			Assert.Equal(-1, sm.Position);
+		}
+
+		[Fact]
+		public void PeekNext()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+
+			// Act
+			var t = sm.PeekNext();
+
+			// Assert
+			Assert.Equal(-1, sm.Position);
+			Assert.Equal(0, t.Id);
+		}
+
+		[Fact]
+		public void PeekPrevious()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Position = 1;
+
+			// Act
+			var t = sm.PeekPrevious();
+
+			// Assert
+			Assert.Equal(1, sm.Position);
+			Assert.Equal(0, t.Id);
+		}
+
+		[Fact]
+		public void Current()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Position = 0;
+
+			// Act
+			var t = sm.Current;
+
+			// Assert
+			Assert.NotNull(t);
+		}
+
+		[Fact]
+		public void Current_Null()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+
+			// Act
+			var t = sm.Current;
+
+			// Assert
+			Assert.Null(t);
+		}
+
+		[Fact]
+		public void VisitAllNext()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Position = 0;
+
+			// Act
+			sm.VisitAllNext((i, t) =>
+				{
+					t.Deposit = string.Empty;
+				});
+
+			// Assert
+			Assert.Null(sm.Data[0].Deposit);
+			Assert.NotNull(sm.Data[1].Deposit);
+		}
+
+		[Fact]
+		public void VisitAllPrevious()
+		{
+			// Arrange
+			var sm = new StateMachine<Test>(new[] { new Test(), new Test() });
+			sm.Position = 1;
+
+			// Act
+			sm.VisitAllPrevious((i, t) =>
+				{
+					t.Deposit = string.Empty;
+				});
+
+			// Assert
+			Assert.NotNull(sm.Data[0].Deposit);
+			Assert.Null(sm.Data[1].Deposit);
+		}
+
+		public void Dispose()
+		{
+			Test.Reset();
+		}
+
+		private class Test
+		{
+			private static int _counter = 0;
+
+			public static void Reset()
+			{
+				_counter = 0;
+			}
+
+			public Test()
+			{
+				Id = _counter++;
+			}
+
+			public int Id { get; private set; }
+
+			public string Deposit { get; set; }
+		}
+	}
+}
