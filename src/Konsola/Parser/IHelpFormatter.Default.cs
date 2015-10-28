d4a51a0d1d022b5044f73bec0cf1d35670bd759a
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-28 15:55:33 +0200

    + IHelpFormatter

---
+++ src/Konsola/Parser/IHelpFormatter.Default.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Konsola.Parser
+{
+	public class DefaultHelpFormatter : IHelpFormatter
+	{
+		public void Format(HelpContext context, IConsole console)
+		{
+			console.WriteLine(context.Options.Description);
+			console.WriteLine();
+			FormatCommands(context.NestedCommands, console);
+			console.WriteLine();
+			FormatParameters(context.Parameters, console);
+		}
+
+		public void FormatForCommand(CommandHelpContext context, IConsole console)
+		{
+			console.WriteLine(context.Attribute.Description);
+			console.WriteLine();
+			FormatCommands(context.NestedCommands, console);
+			console.WriteLine();
+			FormatParameters(context.Parameters, console);
+		}
+
+		private void FormatParameters(IEnumerable<ParameterContext> parameters, IConsole console)
+		{
+			if (!parameters.Any())
+			{
+				return;
+			}
+
+			console.WriteLine(WriteKind.Info, "Parameters:");
+			foreach (var parameter in parameters)
+			{
+				console.WriteLine("* " + parameter.FullName + " - " + parameter.Attribute.Description);
+			}
+		}
+
+		private void FormatCommands(IEnumerable<CommandAttribute> commands, IConsole console)
+		{
+			if (!commands.Any())
+			{
+				return;
+			}
+
+			console.WriteLine(WriteKind.Info, "Commands:");
+			foreach (var command in commands)
+			{
+				console.WriteLine("* " + command.Name + " - " + command.Description);
+			}
+		}
+	}
+}
---
+++ src/Konsola/Parser/IHelpFormatter.cs
@@ -0,0 +1,9 @@
+﻿namespace Konsola.Parser
+{
+	public interface IHelpFormatter
+	{
+		void Format(HelpContext context, IConsole console);
+
+		void FormatForCommand(CommandHelpContext context, IConsole console);
+	}
+}
