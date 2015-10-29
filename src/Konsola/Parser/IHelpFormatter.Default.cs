d847a351340a15862755944226f15bb7e30a40fb
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-29 10:55:50 +0200

    Tweak the default help formatter

--- src/Konsola/Parser/IHelpFormatter.Default.cs
+++ src/Konsola/Parser/IHelpFormatter.Default.cs
@@ -7,47 +7,71 @@ namespace Konsola.Parser
 	{
 		public void Format(HelpContext context, IConsole console)
 		{
-			console.WriteLine(context.Options.Description);
-			console.WriteLine();
-			FormatCommands(context.NestedCommands, console);
-			console.WriteLine();
-			FormatParameters(context.Parameters, console);
+			if (!string.IsNullOrWhiteSpace(context.Options.Description))
+			{
+				console.WriteLine(context.Options.Description);
+				console.WriteLine();
+			}
+			if (context.NestedCommands.Any())
+			{
+				FormatCommands(context.NestedCommands, console);
+				console.WriteLine();
+			}
+			if (context.Parameters.Any())
+			{
+				FormatParameters(context.Parameters, console);
+			}
 		}
 
 		public void FormatForCommand(CommandHelpContext context, IConsole console)
 		{
-			console.WriteLine(context.Attribute.Description);
-			console.WriteLine();
-			FormatCommands(context.NestedCommands, console);
-			console.WriteLine();
-			FormatParameters(context.Parameters, console);
+			if (!string.IsNullOrWhiteSpace(context.Attribute.Description))
+			{
+				console.WriteLine(context.Attribute.Description);
+				console.WriteLine();
+			}
+			if (context.NestedCommands.Any())
+			{
+				FormatCommands(context.NestedCommands, console);
+				console.WriteLine();
+			}
+			if (context.Parameters.Any())
+			{
+				FormatParameters(context.Parameters, console);
+			}
 		}
 
 		private void FormatParameters(IEnumerable<ParameterContext> parameters, IConsole console)
 		{
-			if (!parameters.Any())
-			{
-				return;
-			}
-
 			console.WriteLine(WriteKind.Info, "Parameters:");
 			foreach (var parameter in parameters)
 			{
-				console.WriteLine("* " + parameter.FullName + " - " + parameter.Attribute.Description);
+				console.Write("* " + parameter.FullName);
+				if (!string.IsNullOrWhiteSpace(parameter.Attribute.Description))
+				{
+					console.WriteLine(" - " + parameter.Attribute.Description);
+				}
+				else
+				{
+					console.WriteLine();
+				}
 			}
 		}
 
 		private void FormatCommands(IEnumerable<CommandAttribute> commands, IConsole console)
 		{
-			if (!commands.Any())
-			{
-				return;
-			}
-
 			console.WriteLine(WriteKind.Info, "Commands:");
 			foreach (var command in commands)
 			{
-				console.WriteLine("* " + command.Name + " - " + command.Description);
+				console.Write("* " + command.Name);
+				if (!string.IsNullOrWhiteSpace(command.Description))
+				{
+					console.WriteLine(" - " + command.Description);
+				}
+				else
+				{
+					console.WriteLine();
+				}
 			}
 		}
 	}
